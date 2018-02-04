dd1bd9f6b9063fcf5c7cb5f30c017b0a4151962b
petrov <petrovw@yandex.ru>
2018-02-05 00:49:24 +0300

    Client

--- javaFxClient/src/sample/Controller.java
+++ javaFxClient/src/sample/Controller.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -14,12 +15,11 @@ import java.util.ArrayList;
 public class Controller {
 
     private final String USER_AGENT = "Mozilla/5.0";
-    ArrayList<Good> goods = new ArrayList<>();
 
     public ArrayList<Good> getAll() throws IOException {
         String request = "http://localhost:4567/getAll";
-
         URL url = new URL(request);
+
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", USER_AGENT);
@@ -39,4 +39,31 @@ public class Controller {
         return goods;
     }
 
+    public void buy(ArrayList<Good> goods) throws IOException {
+        String listToJson = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(goods);
+
+        String request = "http://localhost:4567/buy";
+        URL url = new URL(request);
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        String urlParameters = listToJson;
+
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+    }
 }
--- javaFxClient/src/sample/Main.java
+++ javaFxClient/src/sample/Main.java
@@ -10,70 +10,71 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage window) throws Exception {
-        window.setX(100);
-        window.setY(10);
 
-        Controller service = new Controller();
-        ArrayList<Good> goodsInShop = null;
-        try {
-            goodsInShop = service.getAll();
-        } catch (Exception e) {
-            System.out.println("Hello!");
-        }
+        final Logger LOGGER = LoggerFactory.getLogger(Good.class);
+        window.setX(700);
+        window.setY(10);
 
         ScrollPane mainPane = new ScrollPane();
         mainPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
 
         GridPane gridPane = new GridPane();
-        int numberOfGoods = goodsInShop.size();
-
-        gridPane.getCellBounds(5, numberOfGoods + 3);
-
         gridPane.add(new Label(" Наименование "), 0, 0);
         gridPane.add(new Label(" Цена, $"), 1, 0);
         gridPane.add(new Label(" Кол-во в\nмагазине "), 2, 0);
         gridPane.add(new Label(" Кол-во в\nкорзине "), 4, 0);
         gridPane.add(new Label(" Стоимость "), 5, 0);
 
-        Label[] name = new Label[numberOfGoods];
-        Label[] price = new Label[numberOfGoods];
-        Label[] numberInShop = new Label[numberOfGoods];
-        Button[] butPlus = new Button[numberOfGoods];
-        Button[] butMinus = new Button[numberOfGoods];
-        Label[] numberInCart = new Label[numberOfGoods];
-        Label[] cost = new Label[numberOfGoods];
+        Controller service = new Controller();
+        ArrayList<Good> goodsInShop = null;
+        try {
+            goodsInShop = service.getAll();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        int countOfGoods = goodsInShop.size();
+        gridPane.getCellBounds(5, countOfGoods + 3);
         Label totalCost = new Label();
         Label totalCount = new Label();
-        gridPane.add(totalCount, 4, numberOfGoods + 2);
-        gridPane.add(totalCost, 5, numberOfGoods + 2);
-
-        gridPane.add(new Label("КОЛ-ВО"), 4, numberOfGoods + 1);
-        gridPane.add(new Label("СУММА"), 5, numberOfGoods + 1);
+        gridPane.add(totalCount, 4, countOfGoods + 2);
+        gridPane.add(totalCost, 5, countOfGoods + 2);
+        gridPane.add(new Label("КОЛ-ВО"), 4, countOfGoods + 1);
+        gridPane.add(new Label("СУММА"), 5, countOfGoods + 1);
         Button butBuy = new Button();
         butBuy.setText("ОПЛАТА");
         butBuy.setMinWidth(65);
-
-        for (int i = 0; i < numberOfGoods; i++) {
+        
+        Label[] name = new Label[countOfGoods];
+        Label[] price = new Label[countOfGoods];
+        Label[] countInShop = new Label[countOfGoods];
+        Button[] butPlus = new Button[countOfGoods];
+        Button[] butMinus = new Button[countOfGoods];
+        Label[] countInCart = new Label[countOfGoods];
+        Label[] cost = new Label[countOfGoods];
+        
+        for (int i = 0; i < countOfGoods; i++) {
             Good good = goodsInShop.get(i);
             int row = i + 1;
             name[i] = new Label(good.name);
             price[i] = new Label(String.valueOf(good.price));
-            numberInShop[i] = new Label(String.valueOf(good.count));
-            numberInCart[i] = new Label();
+            countInShop[i] = new Label(String.valueOf(good.count));
+            countInCart[i] = new Label();
             cost[i] = new Label();
             gridPane.add(name[i], 0, row);
             gridPane.add(price[i], 1, row);
-            gridPane.add(numberInShop[i], 2, row);
-            gridPane.add(numberInCart[i], 4, row);
+            gridPane.add(countInShop[i], 2, row);
+            gridPane.add(countInCart[i], 4, row);
             gridPane.add(cost[i], 5, row);
-
             butPlus[i] = new Button();
             butPlus[i].setText("+");
             butPlus[i].setMinWidth(65);
@@ -85,22 +86,23 @@ public class Main extends Application {
             VBox boxButton = new VBox();
             boxButton.getChildren().addAll(butPlus[i], butMinus[i]);
             gridPane.add(boxButton, 3, row);
+        }
 
+        for (int i = 0; i < countOfGoods; i++) {
             int index = i;
-
             butPlus[i].setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
-                    if (Integer.parseInt(numberInShop[index].getText()) > 0) {
+                    if (Integer.parseInt(countInShop[index].getText()) > 0) {
                         int numInCart = 0;
-                        if (!numberInCart[index].getText().isEmpty()) {
-                            numInCart = Integer.parseInt(numberInCart[index].getText());
+                        if (!countInCart[index].getText().isEmpty()) {
+                            numInCart = Integer.parseInt(countInCart[index].getText());
                         }
                         numInCart++;
-                        numberInCart[index].setText(String.valueOf(numInCart));
+                        countInCart[index].setText(String.valueOf(numInCart));
                         int currentPrice = Integer.parseInt(price[index].getText());
                         cost[index].setText(String.valueOf(numInCart * currentPrice));
-                        totalCount.setText(sum(numberInCart));
+                        totalCount.setText(sum(countInCart));
                         totalCost.setText(sum(cost));
                     }
                 }
@@ -110,18 +112,18 @@ public class Main extends Application {
                 @Override
                 public void handle(ActionEvent actionEvent) {
                     int numInCart;
-                    if (!numberInCart[index].getText().isEmpty()) {
-                        numInCart = Integer.parseInt(numberInCart[index].getText());
+                    if (!countInCart[index].getText().isEmpty()) {
+                        numInCart = Integer.parseInt(countInCart[index].getText());
                         if (numInCart > 0) {
                             numInCart--;
-                            numberInCart[index].setText(String.valueOf(numInCart));
+                            countInCart[index].setText(String.valueOf(numInCart));
                             int currentPrice = Integer.parseInt(price[index].getText());
                             cost[index].setText(String.valueOf(numInCart * currentPrice));
-                            totalCount.setText(sum(numberInCart));
+                            totalCount.setText(sum(countInCart));
                             totalCost.setText(sum(cost));
                         }
                         if (numInCart == 0) {
-                            numberInCart[index].setText("");
+                            countInCart[index].setText("");
                             cost[index].setText("");
                         }
                     }
@@ -129,22 +131,31 @@ public class Main extends Application {
             });
         }
 
+        ArrayList<Good> goods = goodsInShop;
+        butBuy.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                ArrayList<Good> buyList = new ArrayList<>();
+                for (int index = 0; index < countOfGoods; index++) {
+                    if (!countInCart[index].getText().isEmpty()) {
+                        int count = Integer.parseInt(countInCart[index].getText());
+                        buyList.add(goods.get(index));
+                        buyList.get(buyList.size() - 1).count = count;
+                        countInCart[index].setText("");
+                        cost[index].setText("");
+                    }
+                }
+                try {
+                    service.buy(buyList);
+                    service.getAll();
+                    buyList.clear();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
-        //===================================================================================
-//        butBuy.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent actionEvent) {
-//                for (int index = 0; index < numberOfGoods; i++) {
-//                    if (!numberInCart[index].getText().equals("")) {
-//                        int count = Integer.parseInt(numberInCart[i].getText());
-//                        numberInShop[index].setText(String.valueOf(--numInShop));
-//                        numberInCart[index].setText("");
-//                    }
-//                }
-//            }
-//        });
-        //===================================================================================
-        gridPane.add(butBuy, 3, numberOfGoods + 2);
+        gridPane.add(butBuy, 3, countOfGoods + 2);
 
         mainPane.setContent(gridPane);
         window.setScene(new Scene(mainPane, 400, 450));
@@ -165,4 +176,9 @@ public class Main extends Application {
         }
         return String.valueOf(result);
     }
+
+//    private static void viewUpdate(ArrayList<Good> goods, GridPane gridPane) {
+//
+//
+//    }
 }
