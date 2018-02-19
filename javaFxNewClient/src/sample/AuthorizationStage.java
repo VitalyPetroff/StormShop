94d4a6e9ce0a2bedd011bac6b65fa1e744e8726e
petrov <petrovw@yandex.ru>
2018-02-20 02:04:24 +0300

    javaFX-formConstuction

--- javaFxNewClient/src/sample/AuthorizationStage.java
+++ javaFxNewClient/src/sample/AuthorizationStage.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
@@ -11,9 +12,10 @@ import javafx.stage.Stage;
 public class AuthorizationStage {
 
     public void initialization() {
-        int coordX = 500;
+        int coordX = 700;
         int coordY = 10;
         int gap = 20;
+        int inset = 10;
 
         Stage authorization = new Stage();
         Controller controller = new Controller();
@@ -25,11 +27,11 @@ public class AuthorizationStage {
         GridPane pane = new GridPane();
         pane.setHgap(gap);
         pane.setVgap(gap);
-        pane.setPadding(new Insets(5));
+        pane.setPadding(new Insets(inset, inset, inset, inset));
 
-        pane.add(new Label("Input User Name & Password"), 0, 0, 2, 1);
+        pane.add(new Label("Input Admin Name & Password"), 0, 0, 2, 1);
 
-        pane.add(new Label("User Name"), 0, 1);
+        pane.add(new Label("Admin Name"), 0, 1);
         TextField textLogin = new TextField();
         pane.add(textLogin, 1, 1);
 
@@ -38,6 +40,7 @@ public class AuthorizationStage {
         pane.add(textPassword, 1, 2);
 
         Button buttonLogin = new Button("Login");
+        buttonLogin.setAlignment(Pos.BOTTOM_RIGHT);
         pane.add(buttonLogin, 1, 3);
 
         authorization.setScene(new Scene(pane));
--- javaFxNewClient/src/sample/Main.java
+++ javaFxNewClient/src/sample/Main.java
@@ -1,30 +1,133 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public final String server = "http://localhost:4567/";
+
     @Override
-    public void start(Stage shopWindow) throws Exception{
-        AuthorizationStage authorization = new AuthorizationStage();
-        authorization.initialization();
-
-        GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
-        gridPane.setPadding(new Insets(25,25,25,25));
-
-        Scene scene = new Scene(gridPane);
-        shopWindow.show();
+    public void start(Stage shopWindow) throws Exception {
+        viewInitialization(shopWindow);
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void viewInitialization(Stage shopWindow) {
+        int coordX = 100;
+        int coordY = 10;
+        shopWindow.setX(coordX);
+        shopWindow.setY(coordY);
+
+        GridPane pane = createPane();
+        createServerInfo(pane);
+        createShopView(pane);
+        createAddToShop(pane);
+        createAddToCart(pane);
+        createCartView(pane);
+        createBuy(pane);
+
+
+        shopWindow.setScene(new Scene(pane));
+        shopWindow.show();
+    }
+
+    private GridPane createPane() {
+        int gap = 10;
+        int inset = 10;
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setHgap(gap);
+        pane.setVgap(gap);
+        pane.setPadding(new Insets(inset, inset, inset, inset));
+        return pane;
+    }
+
+    private void createServerInfo(GridPane pane) {
+        Label labelServer = new Label("Server");
+        TextField textServer = new TextField(server);
+        Button refreshButton = new Button("Refresh");
+
+        pane.add(labelServer, 0, 0);
+        pane.add(textServer, 1, 0);
+        pane.add(refreshButton, 2, 0);
+
+        refreshButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+            }
+        });
+
+    }
+
+    private void createShopView(GridPane pane) {
+        pane.add(new Label("Goods in Shop"), 1, 1);
+        TableView<Good> shopTable = new TableView<>();
+        TableColumn<Good, String> name = new TableColumn<>("Name");
+        TableColumn<Good, Integer> price = new TableColumn<>("Price");
+        shopTable.getColumns().addAll(name, price);
+        pane.add(shopTable, 0, 2, 2, 1);
+    }
+
+    private void createAddToShop(GridPane pane) {
+        Button buttonAddToCart = new Button("Add new good");
+        pane.add(buttonAddToCart, 1, 3);
+
+        buttonAddToCart.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                new AuthorizationStage().initialization();
+            }
+        });
+    }
+
+    private void createAddToCart(GridPane pane) {
+        Button buttonAddToCart = new Button("Add to Card");
+        pane.add(buttonAddToCart, 2, 2);
+
+        buttonAddToCart.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+    private void createCartView(GridPane pane) {
+        pane.add(new Label("Goods in Cart"), 4, 1);
+        TableView<Good> cartTable = new TableView<>();
+        TableColumn<Good, String> name = new TableColumn<>("Name");
+        TableColumn<Good, Integer> price = new TableColumn<>("Price");
+        TableColumn<Good, Integer> number = new TableColumn<>("Number");
+        cartTable.getColumns().addAll(name, price, number);
+        pane.add(cartTable, 3, 2, 3, 1);
+        pane.add(new Label("Total number:"), 3, 4);
+        pane.add(new Label("Total price:"), 3, 5);
+    }
+
+    private void createBuy(GridPane pane) {
+        Button buttonAddToCart = new Button("Buy all goods");
+        pane.add(buttonAddToCart, 4, 3);
+
+        buttonAddToCart.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+            }
+        });
     }
 }
